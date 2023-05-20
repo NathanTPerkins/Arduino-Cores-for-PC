6d18ac27ae3e11f94fc8402819f5f5c69af81eed
NathanTPerkins <108106121+NathanTPerkins@users.noreply.github.com>
2023-05-20 16:31:47 -0500

    added serial monitor

---
+++ cores/arduino/HardwareSerial.cpp
@@ -0,0 +1,143 @@
+#include "HardwareSerial.h"
+
+HardwareSerial::HardwareSerial(uint8_t* u1, uint8_t* u2, uint8_t* u3, uint8_t* u4, uint8_t* u5, uint8_t* u6){
+    this->serialMonitor = HardwareSerialFILE;
+}
+
+void HardwareSerial::begin(unsigned long baud, uint8_t config){
+
+}
+
+void HardwareSerial::end(){
+
+}
+
+int HardwareSerial::available(){
+    return 1;
+}
+
+int HardwareSerial::peek(){
+    return 1;
+}
+
+int HardwareSerial::read(){
+    return 1;
+}
+
+int HardwareSerial::availableForWrite(){
+    return 1;
+}
+
+void HardwareSerial::flush(){
+    return;
+}
+
+size_t HardwareSerial::write(uint8_t c){
+    fprintf(this->serialMonitor, "%c", c);
+    return 1;
+}
+
+size_t HardwareSerial::write(const uint8_t*buffer, size_t size){
+    if(buffer == NULL){
+        return 0;
+    }
+    fprintf(this->serialMonitor, "%s", (const char *)buffer);
+    return size;
+}
+
+size_t HardwareSerial::write(const char *str){
+    return write((const uint8_t*)str, strlen(str));
+}
+
+size_t HardwareSerial::print(const String &s){
+    return print(s.c_str());
+}
+
+size_t HardwareSerial::print(const char *s){
+    fprintf(this->serialMonitor, "%s", s);
+    return strlen(s);
+}
+
+size_t HardwareSerial::print(char c){
+    return print(String(c));
+}
+
+size_t HardwareSerial::print(unsigned char c){
+    return print(String(c));
+}
+
+size_t HardwareSerial::print(int i){
+    return print(String(i));
+}
+
+size_t HardwareSerial::print(unsigned int i){
+    return print(String(i));
+}
+
+size_t HardwareSerial::print(long l){
+    return print(String(l));
+}
+
+size_t HardwareSerial::print(unsigned long l){
+    return print(String(l));
+}
+
+size_t HardwareSerial::print(double d){
+    return print(String(d));
+}
+
+size_t HardwareSerial::println(const String &s){
+    return println(s.c_str());
+}
+
+size_t HardwareSerial::println(const char *s){
+    fprintf(this->serialMonitor, "%s\n", s);
+    return strlen(s);
+}
+
+size_t HardwareSerial::println(char c){
+    return println(String(c));
+}
+
+size_t HardwareSerial::println(unsigned char c){
+    return println(String(c));
+}
+
+size_t HardwareSerial::println(int i){
+    return println(String(i));
+}
+
+size_t HardwareSerial::println(unsigned int i){
+    return println(String(i));
+}
+
+size_t HardwareSerial::println(long l){
+    return println(String(l));
+}
+
+size_t HardwareSerial::println(unsigned long l){
+    return println(String(l));
+}
+
+size_t HardwareSerial::println(double d){
+    return println(String(d));
+}
+
+size_t HardwareSerial::println(){
+    fprintf(this->serialMonitor, "\n");
+    return 0;
+}
+
+size_t HardwareSerial::printf(const char *format, ...){
+    va_list args;
+    va_start(args, format);
+    vfprintf(stdout, format, args);
+    va_end(args);
+    return 1;
+}
+
+
+
+
+
+
--- cores/arduino/HardwareSerial.h
+++ cores/arduino/HardwareSerial.h
@@ -3,12 +3,43 @@
 
 #include <stdio.h>
 #include <inttypes.h>
+#include <string.h>
+#include <stdarg.h>
+#include "WString.h"
+
+#define SERIAL_5N1 0x00
+#define SERIAL_6N1 0x02
+#define SERIAL_7N1 0x04
+#define SERIAL_8N1 0x06
+#define SERIAL_5N2 0x08
+#define SERIAL_6N2 0x0A
+#define SERIAL_7N2 0x0C
+#define SERIAL_8N2 0x0E
+#define SERIAL_5E1 0x20
+#define SERIAL_6E1 0x22
+#define SERIAL_7E1 0x24
+#define SERIAL_8E1 0x26
+#define SERIAL_5E2 0x28
+#define SERIAL_6E2 0x2A
+#define SERIAL_7E2 0x2C
+#define SERIAL_8E2 0x2E
+#define SERIAL_5O1 0x30
+#define SERIAL_6O1 0x32
+#define SERIAL_7O1 0x34
+#define SERIAL_8O1 0x36
+#define SERIAL_5O2 0x38
+#define SERIAL_6O2 0x3A
+#define SERIAL_7O2 0x3C
+#define SERIAL_8O2 0x3E
+
+#define HardwareSerialFILE stdout
 
 class HardwareSerial{
 private:
     FILE *serialMonitor;
 public:
-    void begin(unsigned long);
+    HardwareSerial(uint8_t*, uint8_t*, uint8_t*, uint8_t*, uint8_t*, uint8_t*);
+    void begin(unsigned long baud){begin(baud, SERIAL_8N1);};
     void begin(unsigned long, uint8_t);
     void end();
     int available();
@@ -17,7 +48,39 @@ public:
     int availableForWrite();
     void flush();
     size_t write(uint8_t);
-    size_t write(unsigned long);
+    size_t write(unsigned long c) {return write((uint8_t)c);};
+    size_t write(long c) {return write((uint8_t)c);};
+    size_t write(unsigned int c) {return write((uint8_t)c);};
+    size_t write(int c) {return write((uint8_t)c);};
+    
+    //Print::write functions
+    size_t write(const uint8_t*, size_t);
+    size_t write(const char *);
+
+    size_t print(const String&);
+    size_t print(const char *);
+    size_t print(char);
+    size_t print(unsigned char);
+    size_t print(int);
+    size_t print(unsigned int);
+    size_t print(long);
+    size_t print(unsigned long);
+    size_t print(double);
+
+    size_t println(const String&);
+    size_t println(const char *);
+    size_t println(char);
+    size_t println(unsigned char);
+    size_t println(int);
+    size_t println(unsigned int);
+    size_t println(long);
+    size_t println(unsigned long);
+    size_t println(double);
+    size_t println();
+
+    size_t printf(const char *, ...);
+
+    operator bool() {return true;}
 };
 
 #endif
